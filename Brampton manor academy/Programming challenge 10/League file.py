ba92a23b18ebb107482a383335720a32ad1510f6
Sobithan11 <113171415+Sobithan11@users.noreply.github.com>
2022-09-21 14:55:27 +0100

    Add files via upload

---
+++ Brampton manor academy/Programming challenge 10/League file.py
@@ -0,0 +1,7 @@
+import csv
+
+with open('Premier 16-17.csv') as csvfile:
+    spamreader=csv.reader(csvfile, delimiter=',
+    for row in spamreader:
+                          
+                          
